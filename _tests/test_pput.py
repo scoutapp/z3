b9b3069b52d48df5481aefb2cf7434c04dbd6223
Lance T. Erickson <lancetarn@gmail.com>
2023-04-19 16:06:48 -0500

    WIP: Initial 2to3 and simple test work

--- _tests/test_pput.py
+++ _tests/test_pput.py
@@ -1,15 +1,23 @@
-from cStringIO import StringIO
+from io import StringIO
 from datetime import datetime
-from Queue import Queue
+from queue import Queue
 from uuid import uuid4
 import hashlib
 
 import boto
 import pytest
 
-from z3.pput import (UploadSupervisor, UploadWorker, StreamHandler,
-                     Result, WorkerCrashed, multipart_etag, parse_metadata,
-                     retry, UploadException)
+from z3.pput import (
+    UploadSupervisor,
+    UploadWorker,
+    StreamHandler,
+    Result,
+    WorkerCrashed,
+    multipart_etag,
+    parse_metadata,
+    retry,
+    UploadException,
+)
 from z3.config import get_config
 
 
@@ -21,14 +29,17 @@ class ReadOnlyFile(object):
     """A read-only file like object.
     Helps ensure we don't accidentally mutate the fixture between test runs.
     """
-    def __init__(self, fd, allowed=('read', 'seek')):
+
+    def __init__(self, fd, allowed=("read", "seek")):
         self._fd = fd
         self._allowed = set(allowed)
 
     def __getattr__(self, name):
         if name in self._allowed:
             return getattr(self._fd, name)
-        raise AssertionError("this file-like-object is readonly, {} is now allowed".format(name))
+        raise AssertionError(
+            "this file-like-object is readonly, {} is now allowed".format(name)
+        )
 
 
 @pytest.fixture()
@@ -40,18 +51,16 @@ def sample_data():
     global _cached_sample_data
     if _cached_sample_data is None:
         data = StringIO()
-        chars = "".join(chr(i) for i in xrange(256))
-        for count in xrange(6):
+        chars = "".join(chr(i) for i in range(256))
+        for count in range(6):
             cc = chr(count)
-            for _ in xrange(2 * 1024):
+            for _ in range(2 * 1024):
                 # each iteration adds 1MB
                 # each 1MB chunk is made up of an alternation of the block's index (zero based)
                 # and an incrementing counter (overflows to 0 several times)
                 # the first block will be: 00 00 00 01 00 02 ... 00 ff 00 00 ... 00 ff
-                data.write(
-                    "".join(cc+chars[i] for i in xrange(256))
-                )
-        print "wrote {} MB" .format(data.tell() / 1024.0 / 1024.0)
+                data.write("".join(cc + chars[i] for i in range(256)))
+        print("wrote {} MB".format(data.tell() / 1024.0 / 1024.0))
         # give the test a read-only file to avoid accidentally modifying the data between tests
         _cached_sample_data = ReadOnlyFile(data)
     _cached_sample_data.seek(0)
@@ -65,7 +74,7 @@ def test_multipart_etag(sample_data):
         if len(chunk) == 0:
             break
         digest = hashlib.md5()
-        digest.update(chunk)
+        digest.update(chunk.encode("latin1"))
         digests.append(digest.hexdigest())
     assert multipart_etag(digests) == '"d229c1fc0e509475afe56426c89d2724-2"'
 
@@ -76,18 +85,18 @@ def test_stream_handler():
     while not stream_handler.finished:
         chunk = stream_handler.get_chunk()
         chunks.append(chunk)
-    assert chunks == ['aa', 'bb', 'cc', 'dd', 'e']
+    assert chunks == ["aa", "bb", "cc", "dd", "e"]
 
 
 def test_handle_results():
     sup = UploadSupervisor(None, None, None)
     sup.inbox = Queue()
     sup._pending_chunks = 3
-    sup.inbox.put(Result(success=True, traceback=None, index=1, md5='a'))
-    sup.inbox.put(Result(success=True, traceback=None, index=3, md5='c'))
-    sup.inbox.put(Result(success=True, traceback=None, index=2, md5='b'))
+    sup.inbox.put(Result(success=True, traceback=None, index=1, md5="a"))
+    sup.inbox.put(Result(success=True, traceback=None, index=3, md5="c"))
+    sup.inbox.put(Result(success=True, traceback=None, index=2, md5="b"))
     sup._handle_results()
-    assert sorted(sup.results) == [(1, 'a'), (2, 'b'), (3, 'c')]
+    assert sorted(sup.results) == [(1, "a"), (2, "b"), (3, "c")]
     assert sup._pending_chunks == 0
 
 
@@ -101,12 +110,12 @@ class FakeMultipart(object):
 
     def complete_upload(self):
         if self._completed:
-            raise AssertionError('multipart already completed')
+            raise AssertionError("multipart already completed")
         self._completed = True
 
     def cancel_upload(self):
         if self._canceled:
-            raise AssertionError('multipart already canceled')
+            raise AssertionError("multipart already canceled")
         self._canceled = True
 
 
@@ -121,13 +130,13 @@ class FakeBucket(object):
 
 class DummyWorker(UploadWorker):
     def upload_part(self, index, chunk):
-        return hashlib.md5(chunk).hexdigest()
+        return hashlib.md5(chunk.encode("latin1")).hexdigest()
 
 
 def test_supervisor_loop(sample_data):
     stream_handler = StreamHandler(sample_data)
     bucket = FakeBucket()
-    sup = UploadSupervisor(stream_handler, 'test', bucket=bucket)
+    sup = UploadSupervisor(stream_handler, "test", bucket=bucket)
     etag = sup.main_loop(worker_class=DummyWorker)
     assert etag == '"d229c1fc0e509475afe56426c89d2724-2"'
     assert bucket._multipart._completed
@@ -136,7 +145,7 @@ def test_supervisor_loop(sample_data):
 def test_zero_data(sample_data):
     stream_handler = StreamHandler(StringIO())
     bucket = FakeBucket()
-    sup = UploadSupervisor(stream_handler, 'test', bucket=bucket)
+    sup = UploadSupervisor(stream_handler, "test", bucket=bucket)
     with pytest.raises(UploadException):
         sup.main_loop(worker_class=DummyWorker)
     assert bucket._multipart._canceled is True
@@ -152,7 +161,7 @@ class ErrorWorker(UploadWorker):
 def test_supervisor_loop_with_worker_crash(sample_data):
     stream_handler = StreamHandler(sample_data)
     bucket = FakeBucket()
-    sup = UploadSupervisor(stream_handler, 'test', bucket=bucket)
+    sup = UploadSupervisor(stream_handler, "test", bucket=bucket)
     with pytest.raises(WorkerCrashed):
         sup.main_loop(worker_class=ErrorWorker)
 
@@ -174,7 +183,7 @@ class Boom(object):
 def test_retry_decorator():
     boom = Boom()
     with pytest.raises(BoomException) as excp_info:
-        for _ in xrange(3):
+        for _ in range(3):
             boom.call()
     assert boom.count == 3
 
@@ -183,14 +192,15 @@ def test_retry_decorator():
 def test_integration(sample_data):
     cfg = get_config()
     stream_handler = StreamHandler(sample_data)
-    bucket = boto.connect_s3(
-        cfg['S3_KEY_ID'], cfg['S3_SECRET']).get_bucket(cfg['BUCKET'])
+    bucket = boto.connect_s3(cfg["S3_KEY_ID"], cfg["S3_SECRET"]).get_bucket(
+        cfg["BUCKET"]
+    )
     key_name = "z3_test_" + datetime.now().strftime("%Y%m%d_%H-%M-%S")
     sup = UploadSupervisor(
         stream_handler,
         key_name,
         bucket=bucket,
-        headers=parse_metadata(["ana=are+mere", "dana=are=pere"])
+        headers=parse_metadata(["ana=are+mere", "dana=are=pere"]),
     )
     etag = sup.main_loop()
     uploaded = bucket.get_key(key_name)
--- _tests/test_ssh_sync.py
+++ _tests/test_ssh_sync.py
@@ -4,68 +4,76 @@ import pytest
 from z3.ssh_sync import snapshots_to_send, sync_snapshots
 
 
-ALL = ['S_0', 'S_1', 'S_2', 'S_3', 'S_4']
+ALL = ["S_0", "S_1", "S_2", "S_3", "S_4"]
 
 
 HAPPY_PATH = dict(
     # empty remote, send latest; no base snapshot
-    empty_remote=((ALL, []), (None, 'S_4')),
-
+    empty_remote=((ALL, []), (None, "S_4")),
     # first snapshot common to both, send latest, based on S_0
-    first_sent=((ALL, ['S_0']), ('S_0', 'S_4')),
-
+    first_sent=((ALL, ["S_0"]), ("S_0", "S_4")),
     # send latest, based on S_3
-    latest=((ALL, ['S_1', 'S_3']), ('S_3', 'S_4')),
-
+    latest=((ALL, ["S_1", "S_3"]), ("S_3", "S_4")),
     # nothing to do
-    noop=((ALL[1:], ALL), ('S_4', 'S_4')),
+    noop=((ALL[1:], ALL), ("S_4", "S_4")),
 )
 
 
-@pytest.mark.parametrize("pair, expected", HAPPY_PATH.values(), ids=HAPPY_PATH.keys())
+@pytest.mark.parametrize(
+    "pair, expected", list(HAPPY_PATH.values()), ids=list(HAPPY_PATH.keys())
+)
 def test_snapshots_to_send(pair, expected):
     local, remote = pair
     assert snapshots_to_send(local, remote) == expected
 
 
 ERRORS = dict(
-    empty_local=(([], []),
-                 "No snapshots exist locally!"),
-    no_common_snapshots=((["S_0"], ["S_10"]),
-                         "Latest snapshot on destination doesn't exist on source!"),
+    empty_local=(([], []), "No snapshots exist locally!"),
+    no_common_snapshots=(
+        (["S_0"], ["S_10"]),
+        "Latest snapshot on destination doesn't exist on source!",
+    ),
 )
 
 
-@pytest.mark.parametrize('pair, err_msg', ERRORS.values(), ids=ERRORS.keys())
+@pytest.mark.parametrize(
+    "pair, err_msg", list(ERRORS.values()), ids=list(ERRORS.keys())
+)
 def test_snapshots_to_send_error(pair, err_msg):
     local, remote = pair
     with pytest.raises(AssertionError) as err:
         snapshots_to_send(local, remote)
-    assert err_msg == err.value.message
+    assert err_msg in str(err)
 
 
 PULL_HAPPY_PATH = dict(
     incremental=(
-        ('S_0', 'S_4'),
-        ("ssh example.com -C 'sudo zfs send -I remote/fs@S_0 remote/fs@S_4'",
-         'mbuffer -s 128k -m 200m -q | zfs recv -d local/fs')),
+        ("S_0", "S_4"),
+        (
+            "ssh example.com -C 'sudo zfs send -I remote/fs@S_0 remote/fs@S_4'",
+            "mbuffer -s 128k -m 200m -q | zfs recv -d local/fs",
+        ),
+    ),
     empty_target=(
-        (None, 'S_4'),
-        ("ssh example.com -C 'sudo zfs send remote/fs@S_4'",
-         'mbuffer -s 128k -m 200m -q | zfs recv -d local/fs')),
-    noop=(
-        ('S_4', 'S_4'),
-        None),
+        (None, "S_4"),
+        (
+            "ssh example.com -C 'sudo zfs send remote/fs@S_4'",
+            "mbuffer -s 128k -m 200m -q | zfs recv -d local/fs",
+        ),
+    ),
+    noop=(("S_4", "S_4"), None),
 )
 
 
-@pytest.mark.parametrize('pair, expected', PULL_HAPPY_PATH.values(), ids=PULL_HAPPY_PATH.keys())
+@pytest.mark.parametrize(
+    "pair, expected", list(PULL_HAPPY_PATH.values()), ids=list(PULL_HAPPY_PATH.keys())
+)
 def test_pull_command(pair, expected):
     commands = sync_snapshots(
         pair,
-        local_fs='local/fs',
-        remote_fs='remote/fs',
-        remote_addr='example.com',
+        local_fs="local/fs",
+        remote_fs="remote/fs",
+        remote_addr="example.com",
         pull=True,
         dry_run=False,
     )
@@ -74,26 +82,32 @@ def test_pull_command(pair, expected):
 
 PUSH_HAPPY_PATH = dict(
     incremental=(
-        ('S_0', 'S_4'),
-        ("zfs send -I local/fs@S_0 local/fs@S_4",
-         "ssh example.com -C 'mbuffer -s 128k -m 200m -q | sudo zfs recv -d remote/fs'")),
+        ("S_0", "S_4"),
+        (
+            "zfs send -I local/fs@S_0 local/fs@S_4",
+            "ssh example.com -C 'mbuffer -s 128k -m 200m -q | sudo zfs recv -d remote/fs'",
+        ),
+    ),
     empty_target=(
-        (None, 'S_4'),
-        ("zfs send local/fs@S_4",
-         "ssh example.com -C 'mbuffer -s 128k -m 200m -q | sudo zfs recv -d remote/fs'")),
-    noop=(
-        ('S_4', 'S_4'),
-        None),
+        (None, "S_4"),
+        (
+            "zfs send local/fs@S_4",
+            "ssh example.com -C 'mbuffer -s 128k -m 200m -q | sudo zfs recv -d remote/fs'",
+        ),
+    ),
+    noop=(("S_4", "S_4"), None),
 )
 
 
-@pytest.mark.parametrize('pair, expected', PUSH_HAPPY_PATH.values(), ids=PUSH_HAPPY_PATH.keys())
+@pytest.mark.parametrize(
+    "pair, expected", list(PUSH_HAPPY_PATH.values()), ids=list(PUSH_HAPPY_PATH.keys())
+)
 def test_push_command(pair, expected):
     commands = sync_snapshots(
         pair,
-        local_fs='local/fs',
-        remote_fs='remote/fs',
-        remote_addr='example.com',
+        local_fs="local/fs",
+        remote_fs="remote/fs",
+        remote_addr="example.com",
         pull=False,
         dry_run=False,
     )
